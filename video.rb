cc48065a5a1736dfa6d3e7de6a3ea2aa503ddc15
Juan Delgado <juan@ustwo.co.uk>
2013-03-10 16:41:27 +0000

    Now better forcing the colour profile of the output files.
    This prevents weird artifacts when creating the video of those files.

--- video.rb
+++ video.rb
@@ -40,16 +40,21 @@ files.each do |file|
 
 		case ext
 			when GIF
+
 				# In theory, a single call to convert could do both GIF extraction
 				# and resizing, but was giving weird results in several cases.
 				# 2 different calls seem to deal with those cases much better
 				# most likely an issue of my own!
-				system('convert -coalesce ' + file + ' ' + dir + '/frame_%05d.png');
-				system('mogrify -resize 1024x768! -channel RGB ' + dir + '/*.png')
+				
+				# TODO, if less than 48 frames, let's loop the sequence, otherwise
+				# can't almost see it
+				system('convert -coalesce ' + file + ' ' + dir + '/frame_%05d.png')
+				system('mogrify -resize 1024x768! -depth 8 -type TrueColor ' + dir + '/*.png')
 				system('ffmpeg -i ' + dir + '/frame_%05d.png -qscale:v 5 ' + video)
+			
 			when JPG, JPEG, PNG
 				converted_image = dir + '/' + name + '.png'
-				system('convert ' + file  + ' -resize 1024x768! -channel RGB ' + converted_image)
+				system('convert ' + file  + ' -resize 1024x768! -depth 8 -type TrueColor ' + converted_image)
 				system('ffmpeg -loop 1 -f image2 -i ' + converted_image + ' -t 5 -qscale:v 5 ' + video)
 		end
 
@@ -57,10 +62,14 @@ files.each do |file|
 	end
 end
 
-# now stitch all videos together!
-
 puts "Video time..."
 
 system('mv temp/*/*.mpg temp/')
+
+# This way of joining the videos is recommended
+# by FFMPEG. In theory, the cat command alone should
+# suffice, but I've seen MoviePlayer having trouble with the
+# output. The problem disappears doing a final encoding
+# by FFMPEF itself.
 system('cat temp/*.mpg > temp/all.mpg')
 system('ffmpeg -i temp/all.mpg output/gif.mpg')
